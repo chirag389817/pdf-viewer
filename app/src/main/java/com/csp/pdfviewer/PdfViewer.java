65e1d1412802e3be0beac22907e244e147093f44
Chirag <chirag389817@gmail.com>
2023-01-18 10:48:15 +0530

    Basic changes And file Name in action bar

--- app/src/main/java/com/csp/pdfviewer/PdfViewer.java
+++ app/src/main/java/com/csp/pdfviewer/PdfViewer.java
@@ -1,8 +1,13 @@
 package com.csp.pdfviewer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
 
 import com.github.barteksc.pdfviewer.PDFView;
 import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
@@ -14,9 +19,30 @@ public class PdfViewer extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pdf_viewer);
 
+        setMyActionBar();
+
         PDFView pdfView=findViewById(R.id.pdfView);
+        getSupportActionBar().setTitle(getFileName(getIntent().getData()));
         pdfView.fromUri(getIntent().getData())
                 .scrollHandle(new DefaultScrollHandle(this))
                 .load();
+    }
+
+    private void setMyActionBar() {
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @SuppressLint("Range")
+    private String getFileName(Uri uri){
+        String name=null;
+        Cursor cursor=getContentResolver().query(uri,null,null,null,null);
+        try{
+            if(cursor!=null && cursor.moveToFirst())
+                name=cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+        }finally {
+            cursor.close();
+        }
+        return  name;
     }
 }
